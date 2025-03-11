35aaca4bf63eebe0a776815e4a1d9bf717095338
Anna Ro <roanna.97line@gmail.com>
2025-03-11 16:27:01 +0300

    init

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    languageOptions: { globals: globals.browser },
+    rules: {
+      "no-unused-vars": [
+        "error",
+        {
+          vars: "all",
+          args: "after-used",
+          caughtErrors: "all",
+          ignoreRestSiblings: false,
+          reportUsedIgnorePattern: false,
+        },
+      ],
+    },
+  },
+  pluginJs.configs.recommended,
+];
