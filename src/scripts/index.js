2d98556dd83196be5b6e48787abf0cc73f2ec0c5
Anna Ro <roanna.97line@gmail.com>
2025-03-11 18:42:05 +0300

    feat: реализовала открытие/закрытие popup'a

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,17 @@
-console.log("Hello World!");
+import "../index.css";
+
+const popupBu = document.querySelector(".popup__bu");
+const popupOpenBtn = document.querySelector(".popup_btn_open");
+const popupCloseBtn = document.querySelector(".popup_btn_close");
+
+function handleOpenPopup() {
+  console.log("DA");
+  popupBu.classList.add("popup__visible");
+}
+
+function handleClosePopup() {
+  popupBu.classList.remove("popup__visible");
+}
+
+popupOpenBtn.addEventListener("click", handleOpenPopup);
+popupCloseBtn.addEventListener("click", handleClosePopup);
