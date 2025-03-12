876b13e32b335d44df6adef84ec2f1b99fa28676
Anna Ro <roanna.97line@gmail.com>
2025-03-12 14:25:08 +0300

    feat: чистый лист

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,17 +1,1 @@
 import "../index.css";
-
-const popupBu = document.querySelector(".popup__bu");
-const popupOpenBtn = document.querySelector(".popup_btn_open");
-const popupCloseBtn = document.querySelector(".popup_btn_close");
-
-function handleOpenPopup() {
-  console.log("DA");
-  popupBu.classList.add("popup__visible");
-}
-
-function handleClosePopup() {
-  popupBu.classList.remove("popup__visible");
-}
-
-popupOpenBtn.addEventListener("click", handleOpenPopup);
-popupCloseBtn.addEventListener("click", handleClosePopup);
