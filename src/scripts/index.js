02ca9d50f39e0bb8e8f43d7e1182bae466912717
Anna Ro <roanna.97line@gmail.com>
2025-03-14 17:23:05 +0300

    feat: разметка popover и template

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,4 @@
 import "../index.css";
 
 import khokhloma from "../images/khokhloma.svg";
-document.querySelector(".static_side__image").src = khokhloma;
+document.querySelector(".decor__image").src = khokhloma;
